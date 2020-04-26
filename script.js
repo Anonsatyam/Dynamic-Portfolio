0d03f190bc256379de8b48b94a0a14c3fb4b655a
unknown <Anonsatyam>
2020-04-27 00:38:54 +0530

    Sweet Alert Added

--- script.js
+++ script.js
@@ -1,3 +1,10 @@
-$(window).on("load", function () {
-    swal("Here's a message!")
+$(document).ready(function () {
+    swal({
+        title: "Warning",
+        text: "This Website Is Under Construction!",
+        type: "info",
+        confirmButtonColor: '#3085d6',
+        icon: 'warning',
+        timer: 1800
+    });
 });
