0462e75a040ce4011aa43d7e65d89a2f2127025b
unknown <Anonsatyam>
2020-04-27 17:59:57 +0530

    Pushy Button And Light/Dark Mode Added

--- script.js
+++ script.js
@@ -1,10 +1,7 @@
 $(document).ready(function () {
     swal({
-        title: "Warning",
+        title: "Alert",
         text: "This Website Is Under Construction!",
-        type: "info",
-        confirmButtonColor: '#3085d6',
-        icon: 'warning',
-        timer: 1800
+        type: "info"
     });
 });
